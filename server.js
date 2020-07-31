37b9fe61845974e1eee7a5224ab02e11527fe0a0
Admin Jaime <admin.jaime@aall.local>
2020-07-31 14:07:22 -0700

    new server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const sql = require('mssql/msnodesqlv8');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
-const port = 7581;
+const port = 445;
 
 let id;
 let firstName;
