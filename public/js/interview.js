9bf525a838e1d142caf54618e461a743b8bfa342
Admin Jaime <admin.jaime@aall.local>
2020-08-05 16:00:11 -0700

    fixed testing mode

--- public/js/interview.js
+++ public/js/interview.js
@@ -6,12 +6,10 @@ const reqAsync = true;
 
 //parsing the user's UID
 const pathName = window.location.pathname;
-console.log('pathName:', pathName);
+// console.log('pathName:', pathName);
 const splitPath = pathName.split('/');
-console.log('splitPath', splitPath);
+// console.log('splitPath', splitPath);
 const uid = splitPath[2]
-
-// const testVideoID = 'd9f8536e-89db-46de-83fc-648dea3ce5e5';
 
 let request = new XMLHttpRequest();
 
@@ -27,23 +25,20 @@ request.open(method, url, reqAsync);
 request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 //****TESTING**** */
-const testingAPI = () => {
-  console.log('starting testingAPI function');
-  setTimeout(() => {
-    console.log('time ran out');
-    const data = `candUID=${uid}&testVideoID=${testVideoID}`;
-    request.send(data);
-  }, 3000);
-}
+// const testingAPI = () => {
+//   console.log('starting testingAPI function');
+//   setTimeout(() => {
+//     console.log('time ran out');
+//     const data = `candUID=${uid}&testVideoID=${testVideoID}`;
+//     request.send(data);
+//   }, 3000);
+// }
 
 //Will be called after user has completed video.
 const callback = videoID => {
     console.log(`${videoID}`);
-    alert(`About to make request.send with with this videoID: ${videoID}.`)
+    // alert(`About to make request.send with with this videoID: ${videoID}.`)
     const data = `candUID=${uid}&testVideoID=${videoID}`;
     request.send(data);
     window.setTimeout(myInterviewRecorder.reset, 1000)
 }
-
-// $(document).ready(testingAPI);
-
